6e94c3dcfea7429e4dbd2fc4a53b4559a901f981
Allan Reyes <allanbreyes@gmail.com>
2015-03-21 14:56:40 -0500

    add knockout bindings

--- udyssey/static/js/application.js
+++ udyssey/static/js/application.js
@@ -1,20 +1,55 @@
 (function() {
   'use strict';
 
+  /* Fixtures */
+  var fixtureLocations = [
+    "Chicago, IL",
+    "New York, NY",
+    "Austin, TX",
+    "Mountain View, CA"
+  ];
+
+  /* Google Maps Model */
+  var GoogleMap = function() {
+    var self = this;
+
+    var mapCanvas = $('#map-canvas')[0];
+
+    var mapOptions = {
+      center: { lat: 34.397, lng: -90.644},
+      zoom: 4,
+      panControl: false
+    };
+
+    var map = new google.maps.Map(mapCanvas, mapOptions);
+    return map;
+  };
+
   /* Main application view model */
   var AppViewModel = function() {
     var self = this;
     var map;
 
     function initialize() {
-      var mapOptions = {
-          center: { lat: -34.397, lng: 150.644},
-          zoom: 8
-      };
-      map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+      map = new GoogleMap();
+
+      // initialize fixture data
+      fixtureLocations.forEach(function(location) {
+        self.locations.push(location);
+      });
     }
 
-    google.maps.event.addDomListener(window, 'load', initialize);
+    // initialize our observables
+    self.locations = ko.observableArray([]);
+    self.locationInput = ko.observable('');
+
+    // add location function
+    self.addLocation = function() {
+      self.locations.push(self.locationInput());
+      self.locationInput('');
+    };
+
+    $(document).ready(initialize);
   };
 
   ko.applyBindings(new AppViewModel());
